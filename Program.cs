80bbcc3214b53af347c213971fa42b779a75cef2
smoki66 <smoki66@yahoo.com>
2022-11-03 22:01:35 +0100

    proba

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOProba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
